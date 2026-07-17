108e8765778c2928c58270e75026faedcdc63233
JS <rolland.js@gmail.com>
2026-07-17 23:29:53 +0200

    fix: make eslint lint typescript sources

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
 import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx,cjs,mjs}'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module'
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
     rules: {
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
